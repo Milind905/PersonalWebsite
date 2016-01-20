d36ab3601c0276ce7be7f088d5e7e04328156214
Milind Shah <milindS.905@gmail.com>
2016-01-20 02:47:51 -0500

    Created contact page

--- app/scripts/controllers/contactController.js
+++ app/scripts/controllers/contactController.js
@@ -2,5 +2,25 @@ angular.module('personalWebsite')
 .controller('ContactCtrl', function(){
 	var self = this;
 
-	this.test = 'Hello Contact!';
+	self.connections = [{
+		title: 'LinkedIn',
+		iconClass: 'fa-linkedin-square',
+		subtitle: 'Connect',
+		link: "https://ca.linkedin.com/in/mshah905"
+	}, {
+		title: 'Facebook',
+		iconClass: 'fa-facebook-square',
+		subtitle: 'Befriend',
+		link: "https://www.facebook.com/milind905"
+	}, {
+		title: 'Github',
+		iconClass: 'fa-github-square',
+		subtitle: 'Commit',
+		link: "https://github.com/Milind905"
+	}, {
+		title: 'Email',
+		iconClass: 'fa-envelope',
+		subtitle: 'mnshah@uwaterloo.ca',
+		link: "/#/contact"
+	}];
 });
